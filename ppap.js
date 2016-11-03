e069b8a809ca8cc819375744ec50114da2eb6a3f
MrLoki123 <jordiivoetbal@hotmail.com>
2016-11-03 17:16:50 +0100

    yippo
    hippo

--- ppap.js
+++ ppap.js
@@ -5,10 +5,20 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
+const prefix = "<";
+
 client.on('message', message => {
-  reply = message.content.toUpperCase();
-  if (reply === 'DING') {
-     message.reply('DONG') 
-	};
+	if(message.author.bot) return; 
+	
+  
+  if (message.content.startsWith(prefix + "ding")) {
+    message.channel.sendMessage('dong'); 
+	}
+  if (message.content.startsWith(prefix + "cris")) {
+  	message.channel.sendMessage('fuck you!');
+  }
+  if (message.content.startsWith(prefix + "dirk")) {
+  	message.channel.sendMessage('DORK!');
+  }
 });
 client.login('');
